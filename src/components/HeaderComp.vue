32dda3a92cf8677819e91481e02a50585c2fa4fa
csgomezo <carloss.gomezo@utadeo.edu.co>
2020-07-22 14:43:17 -0500

    arreglos al login

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,69 +1,103 @@
 <template>
   <header>
-      <nav class="navbar navbar-expand-lg navbar-light bg-ligth px-md-4 fixed-top">
-        <router-link to="/Home" class="navbar-brand mb-0 h1">
-          <img src="../assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-          Valencia.app
-        </router-link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarCollapse"
-          aria-controls="navbarCollapse"  
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                <router-link to="/Home" class="nav-link">Home</router-link>
-              </li>
-              <li class="nav-item">
-                <router-link to="/Usuarios" class="nav-link">Usuarios</router-link>
-              </li>
-              <li class="nav-item">
-                  <router-link to="/Productos" class="nav-link">Productos</router-link>
-                  
-              </li>
-              <li v-if="auth=='loggedin'" class="nav-item">
-                <router-link to="/" class="nav-link">Perfil</router-link>
-              </li>
-          </ul>
-          <ul class="nav justify-content-end">
-              <li class="nav-item active border border-light rounded" style="margin-right: 5px;">
-                <router-link class="btn btn-dark" to="/Singin" data-toggle="tooltip" data-placement="bottom" title="Tooltip on bottom">Sing in<span class="sr-only">(current)</span></router-link>
-              </li>
-              <li class="nav-item active border border-light rounded">
-                <router-link class="btn btn-dark" to="/SingUp">Sing up<span class="sr-only">(current)</span></router-link>  
-              </li>
-              <li v-if="auth=='loggedin'" class="nav-item">
-                <a to="/" class="nav-link">Logout</a>
-              </li>
-          </ul>
-        </div>
-      </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-ligth px-md-4 fixed-top">
+      <router-link to="/Home" class="navbar-brand mb-0 h1">
+        <img src="../assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt />
+        Valencia.app
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link to="/Home" class="nav-link">Home</router-link>
+          </li>
+          <li v-if="auth=='loggedin'" class="nav-item">
+            <router-link to="/Usuarios" class="nav-link">Usuarios</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/Productos" class="nav-link">Productos</router-link>
+          </li>
+          <li v-if="auth=='loggedin'" class="nav-item">
+            <router-link to="/" class="nav-link">Perfil</router-link>
+          </li>
+        </ul>
+        <ul class="nav justify-content-end">
+          <li v-if="auth==''" class="nav-item active border border-light rounded" style="margin-right: 5px;">
+            <router-link
+              class="btn btn-dark"
+              to="/Singin"
+              data-toggle="tooltip"
+              data-placement="bottom"
+              title="Tooltip on bottom"
+            >
+              Sing in
+              <span class="sr-only">(current)</span>
+            </router-link>
+          </li>
+          <li v-if="auth==''" class="nav-item active border border-light rounded">
+            <router-link class="btn btn-dark" to="/SingUp">
+              Sing up
+              <span class="sr-only">(current)</span>
+            </router-link>
+          </li>
+          <li v-if="auth=='loggedin'" class="nav-item">
+            <a href="/" class="btn btn-secondary btn-lg active" v-on:click="logout()">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </header>
 </template>
 
 <script>
+import EventBus from "./EventBus";
+
+EventBus.$on("logged-in", test => {
+  console.log(test);
+});
+
 export default {
-    name:'HeaderComp'
+  name: "HeaderComp",
+  data() {
+    return {
+      auth: "",
+      usuario: ""
+    };
+  },
+
+  methods: {
+    logout() {
+      localStorage.removeItem("usertoken");
+    }
+  },
+  mounted() {
+    EventBus.$on("logged-in", status => {
+      this.auth = status;
+    });
+  }
 };
 </script>
 
 <style>
 nav {
-  box-shadow: 0px 9px 7px 0px rgba(0,0,0,0.15);
+  box-shadow: 0px 9px 7px 0px rgba(0, 0, 0, 0.15);
   background-color: whitesmoke;
 }
-.nav-item{
-  border-bottom: 2px solid rgba(0,0,0,0);
+.nav-item {
+  border-bottom: 2px solid rgba(0, 0, 0, 0);
   transition: 500ms;
 }
-.nav-item:hover{
+.nav-item:hover {
   border-bottom: 2px solid #cecaca;
 }
 </style>
--- src/components/SingIn.vue
+++ src/components/SingIn.vue
@@ -4,7 +4,7 @@
 
     <div class="row">
       <div class="col-md-6 mt-5 mx-auto">
-        <form v-on:submit.prevent="login()" class="form-signin">
+        <form @submit.prevent="login()" class="form-signin">
           <h1 class="h3 mb-3 font-weigth-normal">Por favor inicia sesión</h1>
           <div class="form-group">
             <label for="correo">Dirección de correo</label>
@@ -36,45 +36,53 @@
 <script>
 import axios from "axios";
 import Global from "../Global";
-import Usuario from "./Models/Usuario";
-import EventBus from './EventBus.vue';
-
+import EventBus from "./EventBus.vue";
+import swal from 'sweetalert'
 
 export default {
   name: "SingIn",
-
-  methods:{
-      login(){
-          axios.post(this.url+'login',{
-              correo:this.correo,
-              contrasenna:this.contrasenna,
-          }).then(res=>{
-              if(res){
-                  
-                  localStorage.setItem('usertoken',res.data)
-                  console.log(localStorage)
-                  this.correo =''
-                  this.contrasenna=''
-                  this.$router.push('/Home');
-              }
-          }).catch(err=>{
-              console.log(err);
-          })
-          this.emitMethod()
-      },
-    emitMethod(){
-        EventBus.$emit('logged-in','loggedin')
-    }
-  },
-  mounted() {},
-
   data() {
     return {
       url: Global.url,
-      correo:'',
-      contrasenna:'',
-      usuario: new Usuario("", "", "", "")
+      correo: '',
+      contrasenna: '',
     };
-  }
+  },
+  methods: {
+    login() {
+      axios
+        .post(this.url + "login",{
+          correo:this.correo, contrasenna:this.contrasenna}
+          )
+        .then(res => {
+          console.log(res.status)
+          console.log(res.data.status)
+          
+          if(res.status==200){
+          localStorage.setItem("usertoken", res.data)
+          console.log(localStorage)
+          this.correo = ""
+          this.contrasenna = ""
+          this.$router.push("/Home")
+          this.emitMethod();
+          }else{
+            this.correo = ""
+            this.contrasenna = ""
+            swal(res.message)
+          }
+        })
+        .catch(err => {
+          this.correo = ""
+          this.contrasenna = ""
+          swal(''+err.response.data.message)
+          console.log(err.response);
+        });
+      
+    },
+    emitMethod() {
+      EventBus.$emit("logged-in", "loggedin");
+    }
+  },
+  mounted() {}
 };
 </script>
